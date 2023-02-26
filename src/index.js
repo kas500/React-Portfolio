570e3f985b0984210c3403a92b7a52fd989e4a4f
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-02-25 20:14:52 -0800

    import tachyons css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import 'purecss/build/base-min.css';
+import 'tachyons/css/tachyons.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
