d17ed98c824c821df3ddbec0f96033c5cc485194
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-02-25 16:00:44 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import 'purecss/build/base-min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
