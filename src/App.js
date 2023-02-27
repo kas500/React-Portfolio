0e5300729b2007c55476bce201c99f8d9db8290e
Anton Krasnikov <krasnikovanton84@gmail.com>
2023-02-26 21:36:04 -0800

    Adds Header Navigation and Footer to return

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-const App = () => <PortfolioContainer />;
+function App()
+{
+    return (
+        <div>
+            <Header />
+            <Navigation />
+            <Footer />
+        </div>
+        )
+}
 
 export default App;
